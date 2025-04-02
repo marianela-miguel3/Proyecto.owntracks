4ce15887e0d1c60e5d606e3b7512e37d61775913
marianela-miguel3 <82850065+marianela-miguel3@users.noreply.github.com>
2025-04-01 22:42:46 -0300

    no cambie nada

--- server.py
+++ server.py
@@ -33,5 +33,5 @@ def get_locations():
     return jsonify(ubicaciones)
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=5000, debug=True)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
