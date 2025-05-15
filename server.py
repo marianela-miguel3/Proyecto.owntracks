75781442687b5b0f00ecd0cc6dafa4f8efb27bd1
marianela-miguel3 <82850065+marianela-miguel3@users.noreply.github.com>
2025-05-15 09:06:53 -0300

    cambios en server

--- server.py
+++ server.py
@@ -24,81 +24,6 @@ def home():
     return "✅ Servidor Flask en Heroku funcionando correctamente 🚀", 200
 
 
-# @app.route('/ubicacion', methods=['POST'])
-# def recibir_ubicacion():
-#     data = request.json
-#     print("📍 Datos recibidos:", data)
-
-#     if not data:
-#         return jsonify({"error": "No se recibieron datos"}), 400
-
-#     if data.get("_type") != "location":
-#         print("🔁 Ignorando mensaje: no es de tipo 'location'")
-#         return jsonify({"status": "ignored"}), 200
-
-#     lat = data.get("lat")
-#     lon = data.get("lon")
-#     timestamp = data.get("tst")
-#     zona = data.get("desc") or (data.get("inregions")[0] if data.get("inregions") else None)
-
-#     if lat is None or lon is None:
-#         print("⚠️ Error: Faltan coordenadas en el mensaje.")
-#         return jsonify({"error": "latitud o longitud faltante"}), 400
-
-#     print("📌 Mensaje de LOCALIZACIÓN detectado")
-#     print("🌍 Latitud:", lat)
-#     print("🌍 Longitud:", lon)
-#     print("🏠 Inregions:", data.get("inregions"))
-#     print("🔋 Batería:", data.get("batt"))
-#     print("🕒 Timestamp:", timestamp)
-#     print("Zona: ", zona)
-#     print("🧭 Dirección (t):", data.get("t"))
-#     print("🆔 TID:", data.get("tid"))
-
-#     # Aquí podrías guardar la información en una base de datos si lo deseas
-
-#     fecha = (
-#         datetime.fromtimestamp(timestamp, tz=timezone.utc)
-#         .astimezone(ARGENTINA_TZ)
-#         .isoformat()
-#     ) if timestamp else None
-
-#     payload = {
-#         "latitud": lat,
-#         "longitud": lon,
-#         "timestamp": fecha
-#     }
-
-#     headers = {
-#         "apikey": SUPABASE_KEY,
-#         "Authorization": f"Bearer {SUPABASE_KEY}",
-#         "Content-Type": "application/json",
-#         "Prefer": "return=representation"
-#     }
-
-#     try:
-#         response = requests.post(
-#             f"{SUPABASE_URL}/rest/v1/ubicaciones",
-#             json=payload,
-#             headers=headers
-#         )
-
-#         print("🔄 Respuesta de Supabase:", response.status_code, response.text)
-
-#         if response.status_code >= 400:
-#             return jsonify({"error": "Error al insertar en Supabase", "detalle": response.text}), response.status_code
-
-#         # return jsonify({"status": "ok", "supabase_response": response.json()}), 201
-#         return jsonify({
-#           "status": "ok",
-#           "supabase_status": response.status_code,
-#           "supabase_text": response.text
-#         }), 201
-
-#     except Exception as e:
-#         print("❌ Error al conectar con Supabase:", str(e))
-#         return jsonify({"error": "Error al conectar con Supabase"}), 500
-
 @app.route('/ubicacion', methods=['POST'])
 def recibir_ubicacion_ou_transicion():
     data = request.json
@@ -122,11 +47,17 @@ def recibir_ubicacion_ou_transicion():
         print("⚠️ Error: Faltan coordenadas en el mensaje de ubicación.")
         return jsonify({"error": "latitud o longitud faltante"}), 400
 
+    # fecha = (
+    #     datetime.fromtimestamp(timestamp, tz=timezone.utc)
+    #     .astimezone(ARGENTINA_TZ)
+    #     .isoformat()
+    # ) if timestamp else None
     fecha = (
-        datetime.fromtimestamp(timestamp, tz=timezone.utc)
-        .astimezone(ARGENTINA_TZ)
-        .isoformat()
+    datetime.fromtimestamp(timestamp, tz=timezone.utc)
+    .astimezone(ARGENTINA_TZ)
+    .strftime("%Y-%m-%d %H:%M")
     ) if timestamp else None
+
 
     print("🧭 Tipo:", tipo)
     print("🌍 Lat:", lat)
